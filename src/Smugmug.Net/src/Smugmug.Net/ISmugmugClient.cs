7950974d2386c4ec154aeafb8dc94ca2ed438a3d
Luke Foust <luke@foust.com>
2015-09-23 13:28:10 -0700

    Initial structure of classes and interfaces

---
+++ src/Smugmug.Net/src/Smugmug.Net/ISmugmugClient.cs
@@ -0,0 +1,41 @@
+﻿namespace Smugmug
+{
+    using Microsoft.Framework.Configuration;
+    using Microsoft.Framework.OptionsModel;
+    using Smugmug.Http;
+
+    public interface ISmugmugClient
+    {
+    }
+
+    public class SmugmugClient : ISmugmugClient
+    {
+        public SmugmugClientConfiguration Configuration { get; set; }
+
+        private readonly IHttpClient http;
+
+        public SmugmugClient(IHttpClient http, IOptions<SmugmugClientConfiguration> options)
+        {
+            this.Configuration = options.Options;
+            this.http = http;
+        }
+    }
+
+    public class SmugmugClientConfiguration
+    {
+    }
+}
+
+namespace Smugmug.Http
+{
+    public interface IHttpClient
+    {
+    }
+}
+
+namespace Smugmug.Models
+{
+    public class User
+    {
+    }
+}
